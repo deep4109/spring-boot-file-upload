5f0486fc71599255f2a79e102d5f92bf165a6471
deep4109 <singhdeepankar41@gmail.com>
2024-10-01 14:00:33 +0530

    file upload

---
+++ src/main/java/com/example/spring_boot_file_upload/SpringBootFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_file_upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileUploadApplication.class, args);
+	}
+
+}
